b6cbdd6f264fceb1bccc53f4f59254991914100a
Amardeep <adssidhu86@gmail.com>
2025-07-09 18:38:55 +0530

    fixing auth

--- auth.ts
+++ auth.ts
@@ -4,6 +4,7 @@ import GitHub from "next-auth/providers/github";
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
   secret: process.env.AUTH_SECRET,
+  trustHost: true, // Required for Amplify and production deployments
   providers: [
     Google({
       clientId: process.env.AUTH_GOOGLE_ID,
