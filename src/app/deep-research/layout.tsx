31eada7fdda3b66f27e8b11eff4e7631b6ef16a0
rutwik187 <rutwikshinde34@gmail.com>
2025-08-28 14:38:41 +0530

    fixed auth issue

--- src/app/deep-research/layout.tsx
+++ src/app/deep-research/layout.tsx
@@ -1,6 +1,4 @@
 import { DeepResearchNavigation } from "@/components/DeepResearch/DeepResearchNavigation";
-import { SessionProvider } from "@/components/providers/SessionProvider";
-import { ThemeProvider } from "@/components/providers/theme-provider";
 
 export default function DeepResearchLayout({
   children,
@@ -8,18 +6,9 @@ export default function DeepResearchLayout({
   children: React.ReactNode;
 }) {
   return (
-    <SessionProvider>
-      <ThemeProvider
-        attribute="class"
-        defaultTheme="system"
-        enableSystem
-        disableTransitionOnChange
-      >
-        <div className="h-screen flex flex-col">
-          <DeepResearchNavigation />
-          <main className="flex-1 overflow-hidden">{children}</main>
-        </div>
-      </ThemeProvider>
-    </SessionProvider>
+    <div className="h-screen flex flex-col">
+      <DeepResearchNavigation />
+      <main className="flex-1 overflow-hidden">{children}</main>
+    </div>
   );
 }
